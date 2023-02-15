891dd485da321cb2bc3134ff5400deface4031eb
liweihua-gk <110764327+liweihua-gk@users.noreply.github.com>
2023-02-15 16:05:17 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # 隐藏made with streamlit
 hide_streamlit_style = """
